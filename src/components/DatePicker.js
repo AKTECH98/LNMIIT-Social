30601b2bce4cf7144c140fc256f4c99a4b7f7902
AbhayAravinda <33093450+AbhayAravinda@users.noreply.github.com>
2020-04-23 12:42:27 +0530

    Added Datepicker component

---
+++ src/components/DatePicker.js
@@ -0,0 +1,30 @@
+import React, { Fragment, useState } from "react";
+import DateFnsUtils from '@date-io/date-fns';
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+
+function BasicDateTimePicker(props) {
+  const [selectedDate, handleDateChange] = useState(props.defaultValue==undefined?null:props.defaultValue);
+
+  const{
+    label,
+    format,
+    returnValue
+  }=props;
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <DatePicker
+        label={label}
+        clearable
+        format={format}
+        value={selectedDate}
+        onChange={(date)=>{
+                     handleDateChange(date);
+                     returnValue(date);
+                 }}
+      />
+    </MuiPickersUtilsProvider>
+  );
+}
+
+export default BasicDateTimePicker;
