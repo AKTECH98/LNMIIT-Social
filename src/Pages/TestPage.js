79d423fbf1474b3fdc6237b038686bfbff44ba1a
anshulkiyawat <17ucs030@lnmiit.ac.in>
2020-06-19 18:16:30 +0530

    HeaderModify

--- src/Pages/TestPage.js
+++ src/Pages/TestPage.js
@@ -1,228 +1,24 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { SearchBar } from 'react-native-elements';
 
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import IconButton from '@material-ui/core/IconButton';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+export default class Test extends React.Component{
+  state = {
+    search: '',
+  };
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: '#20222b',
-    color: 'white',
-    width: 250,
-    height: 250
-  },
-  title: {
-    fontSize : 20
-  },
-  subheader: {
-    color: 'gray',
-    fontSize : 15
-  },
-  content: {
-    fontSize: 15
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+
+  render() {
+    const { search } = this.state;
+
+    return (
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={this.updateSearch}
+        value={search}
+      />
+    );
   }
-}));
-
-export default function RecipeReviewCard() {
-  const classes = useStyles();
-
-  return (
-    <div className = "Widget__View">
-    <Card className={classes.root}>
-      <CardHeader classes={
-        {
-          title : classes.title,
-          subheader : classes.subheader
-        }
-      }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="LNMIIT Social"
-        subheader="Mentor: Mr Vikas Bajpai"
-      />
-      <CardContent classes = {
-        {
-          root: classes.content
-        }
-      }>
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-      </CardContent>
-    </Card>
-    <Card className={classes.root}>
-      <CardHeader classes={
-        {
-          title : classes.title,
-          subheader : classes.subheader
-        }
-      }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="LNMIIT Social"
-        subheader="Mentor: Mr Vikas Bajpai"
-      />
-      <CardContent classes = {
-        {
-          root: classes.content
-        }
-      }>
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-      </CardContent>
-    </Card>
-    <Card className={classes.root}>
-      <CardHeader classes={
-        {
-          title : classes.title,
-          subheader : classes.subheader
-        }
-      }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="LNMIIT Social"
-        subheader="Mentor: Mr Vikas Bajpai"
-      />
-      <CardContent classes = {
-        {
-          root: classes.content
-        }
-      }>
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-      </CardContent>
-    </Card>
-    <Card className={classes.root}>
-      <CardHeader classes={
-        {
-          title : classes.title,
-          subheader : classes.subheader
-        }
-      }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="LNMIIT Social"
-        subheader="Mentor: Mr Vikas Bajpai"
-      />
-      <CardContent classes = {
-        {
-          root: classes.content
-        }
-      }>
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-      </CardContent>
-    </Card>
-    <Card className={classes.root}>
-      <CardHeader classes={
-        {
-          title : classes.title,
-          subheader : classes.subheader
-        }
-      }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="LNMIIT Social"
-        subheader="Mentor: Mr Vikas Bajpai"
-      />
-      <CardContent classes = {
-        {
-          root: classes.content
-        }
-      }>
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-      </CardContent>
-    </Card>
-    <Card className={classes.root}>
-      <CardHeader classes={
-        {
-          title : classes.title,
-          subheader : classes.subheader
-        }
-      }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="LNMIIT Social"
-        subheader="Mentor: Mr Vikas Bajpai"
-      />
-      <CardContent classes = {
-        {
-          root: classes.content
-        }
-      }>
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-      </CardContent>
-    </Card>
-    <Card className={classes.root}>
-      <CardHeader classes={
-        {
-          title : classes.title,
-          subheader : classes.subheader
-        }
-      }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="LNMIIT Social"
-        subheader="Mentor: Mr Vikas Bajpai"
-      />
-      <CardContent classes = {
-        {
-          root: classes.content
-        }
-      }>
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-      </CardContent>
-    </Card>
-    <Card className={classes.root}>
-      <CardHeader classes={
-        {
-          title : classes.title,
-          subheader : classes.subheader
-        }
-      }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="LNMIIT Social"
-        subheader="Mentor: Mr Vikas Bajpai"
-      />
-      <CardContent classes = {
-        {
-          root: classes.content
-        }
-      }>
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-      </CardContent>
-    </Card>
-    </div>
-  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,39 @@ import { Link } from 'react-router-dom';
 
 import Button from './Button'
 
-const Header = () => (
-  <div className = "header">
-    <p className = "header__title">
-      <Link to="/" className = "button--link">
+export default function Header(props){
+  return (
+    <div className = "header">
+      <p className = "header__title">
+        <Link to="/" className = "button--link">
         LNMIIT SOCIAL
-      </Link>
-    </p>
-    <div>
-      <Link to='/Login'>
-        <Button text='Login' type = 'button__header'/>
-      </Link>
-      <Link to='/SignUp'>
-        <Button text='Connect' type = 'button__header'/>
-      </Link>
-    </div>   
-  </div>
-);
-
-export default Header;
+        </Link>
+      </p>
+      {
+        (!props.logout)? // If fasle show the logout and other buttons in
+        <div>
+          <Link to='/Login'>
+            <Button text='Login' type = 'button__header'/>
+          </Link>
+          <Link to='/SignUp'>
+            <Button text='Connect' type = 'button__header'/>
+          </Link>
+        </div>
+        :
+        <div>
+          <Link to='/Home'>
+            <Button text='Home' type = 'button__header'/>
+          </Link>
+          <Link to='/ProfilePage'>
+            <Button text='My Profile' type = 'button__header'/>
+          </Link>
+          <Link to='/Projects'>
+            <Button text='Projects' type = 'button__header'/>
+          </Link>
+          <Button text='Hacks' type = 'button__header'/>
+          <Button text='Logout' type = 'button__header'/>
+        </div>
+      }   
+    </div>
+  );
+}
