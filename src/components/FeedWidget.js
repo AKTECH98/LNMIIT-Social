e42c03d0e45e944d95330e247b7b04f70ddbba7c
tarungupta1999 <17ucs168@lnmiit.ac.in>
2020-11-17 21:24:01 +0530

    FeedWidget correction

--- src/components/FeedWidget.js
+++ src/components/FeedWidget.js
@@ -1,32 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import Button from './Button';
-import PostModal from './PostModal';
+import Button from "./Button";
+import PostModal from "./PostModal";
 
-import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardContent, Typography} from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-import PhotoIcon from '@material-ui/icons/Photo';
-import VideocamIcon from '@material-ui/icons/Videocam';
-import DescriptionIcon from '@material-ui/icons/Description';
-import Dropzone from 'react-dropzone'
-
-
+import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import PhotoIcon from "@material-ui/icons/Photo";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import DescriptionIcon from "@material-ui/icons/Description";
+import Dropzone from "react-dropzone";
 
 const useStyles = makeStyles({
-	root: {
-		backgroundColor: '#20222b',
-		height: 'fit-content',
-		textDecoration: 'none',
-		marginBottom: 20
-	},
-	rootIcon: {
+  root: {
+    backgroundColor: "#20222b",
+    height: "fit-content",
+    textDecoration: "none",
+    marginBottom: 20,
+  },
+  rootIcon: {
+    color: "white",
+  },
 
-		color: 'white',
-	},
-
-
-  	root: {
+  /* root: {
 		backgroundColor: 'white',
 		height: 'fit-content',
 		textDecoration: 'none',
@@ -38,186 +34,137 @@ const useStyles = makeStyles({
   	rootIcon: {
     	color: '#4574bf'
 	},*/
-	rootContent: {
-		display: 'flex',
-		height: 'fit-content',
-		justifyContent: 'space-between',
-		color: 'white'
-	}
+  rootContent: {
+    display: "flex",
+    height: "fit-content",
+    justifyContent: "space-between",
+    color: "white",
+  },
 });
 
 const FeedWidgetView = (props) => (
-	<Card className={useStyles().root}>
-		<CardContent classes={{ root: useStyles().rootContent }}>
-			<Button text="Write a Post" type="post__button" onClick={props.newPost} />
-			{
-				(props.openModal) ?
-					<PostModal
-						openModal={props.openModal}
-						discardPost={props.discardPost}
-						addPost={props.addPost}
-					/>
-					:
-					''
-			}
+  <Card className={useStyles().root}>
+    <CardContent classes={{ root: useStyles().rootContent }}>
+      <Button text="Write a Post" type="post__button" onClick={props.newPost} />
+      {props.openModal ? (
+        <PostModal
+          openModal={props.openModal}
+          discardPost={props.discardPost}
+          addPost={props.addPost}
+        />
+      ) : (
+        ""
+      )}
 
-			<Typography>
+      <Typography>
+        <Dropzone
+          onDrop={props.onDrop}
+          accept="image/jpg, image/png, image/gif" //whatever the file type needed
+          multiple
+        >
+          {({ getRootProps, getInputProps }) => {
+            return (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
 
-				<Dropzone
-					onDrop={props.onDrop}
-					accept="image/jpg, image/png, image/gif"//whatever the file type needed
-
-					multiple
-				>
-					{({
-						getRootProps,
-						getInputProps,
-					}) => {
-
-						return (
-							<div {...getRootProps()}>
-								<input {...getInputProps()} />
-
-								<IconButton classes={{ root: useStyles().rootIcon }} onClick={props.imageUpload}>
-									<PhotoIcon fontSize="large" />
-								</IconButton>
-								<IconButton classes={{ root: useStyles().rootIcon }}>
-									<VideocamIcon fontSize="large" />
-								</IconButton>
-								<IconButton classes={{ root: useStyles().rootIcon }}>
-									<DescriptionIcon fontSize="large" />
-								</IconButton>
-							</div>
-						);
-					}}
-				</Dropzone>
-
-			</Typography>
-
-
-		</CardContent>
-	</Card>
-)
+                <IconButton
+                  classes={{ root: useStyles().rootIcon }}
+                  onClick={props.imageUpload}
+                >
+                  <PhotoIcon fontSize="large" />
+                </IconButton>
+                <IconButton classes={{ root: useStyles().rootIcon }}>
+                  <VideocamIcon fontSize="large" />
+                </IconButton>
+                <IconButton classes={{ root: useStyles().rootIcon }}>
+                  <DescriptionIcon fontSize="large" />
+                </IconButton>
+              </div>
+            );
+          }}
+        </Dropzone>
+      </Typography>
+    </CardContent>
+  </Card>
+);
 
 export default class FeedWidget extends React.Component {
-	state = {
-		openModal: false,
-		openImageUploadModal: false,
-		post: " ",
-		file: "",
-		imagePreviewUrl: "",
-	}
+  state = {
+    openModal: false,
+    openImageUploadModal: false,
+    post: " ",
+    file: "",
+    imagePreviewUrl: "",
+  };
 
-	newPost = () => {
-		this.setState(() => ({
-			openModal: true,
+  newPost = () => {
+    this.setState(() => ({
+      openModal: true,
+    }));
+  };
 
-		}));
-	}
+  discardPost = () => {
+    this.setState(() => ({
+      openModal: false,
+    }));
+  };
 
-	discardPost = () => {
-		this.setState(() => ({
-			openModal: false
-		}));
-	}
+  addPost = (post) => {
+    this.setState(() => ({
+      post,
+      openModal: false,
+    }));
+  };
+  imageChange = (e) => {
+    e.preventDefaule();
+    let reader = new FileReader();
+    e.targer.files[0];
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result,
+      });
+    };
+    reader.readAsDataURL(file);
+  };
+  imageUpload = () => {
+    this.setState(() => ({
+      openImageUploadModal: true,
+    }));
+  };
 
-	addPost = (post) => {
-		this.setState(() => ({
-			post,
-			openModal: false
-		}));
-	}
-	imageChange = e => {
-		e.preventDefaule();
-		let reader = new FileReader();
-		e.targer.files[0];
-		reader.onloadend = () => {
-			this.setState({
-				file: file,
-				imagePreviewUrl: reader.result,
-			});
-		}
-		reader.readAsDataURL(file);
-	}
-	imageUpload = () => {
-		this.setState(() => ({
-			openImageUploadModal: true,
+  onDrop = (files) => {
+    const reader = new FileReader();
+    let file = files[0];
 
-		}));
-	}
+    reader.onabort = () => console.log("file reading was aborted");
+    reader.onerror = () => console.log("file reading has failed");
+    reader.onloadend = () => {
+      console.log("reader result", reader.result);
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result,
+      });
 
+      // console.log(binaryStr)
+    };
+    reader.readAsDataURL(file);
 
-	onDrop = (files) => {
+    // console.log(files);
+  };
 
-		const reader = new FileReader()
-		let file = files[0];
-
-
-		reader.onabort = () => console.log('file reading was aborted')
-		reader.onerror = () => console.log('file reading has failed')
-		reader.onloadend = () => {
-			console.log("reader result", reader.result)
-			this.setState({
-				file: file,
-				imagePreviewUrl: reader.result
-			})
-
-			// console.log(binaryStr)
-		}
-		reader.readAsDataURL(file);
-
-		// console.log(files);
-
-	}
-
-
-	render() {
-
-		if (this.state.imagePreviewUrl) {
-			return (
-				<div>
-					<FeedWidgetView
-						openModal={this.state.openModal}
-						newPost={this.newPost}
-						discardPost={this.discardPost}
-						addPost={this.addPost}
-						imageUpload={this.imageUpload}
-						onDrop={this.onDrop}
-						$imagePreview={this.$imagePreviewUrl}
-					/>
-					<img src={this.state.imagePreviewUrl} />
-
-
-
-				</div>
-			)
-
-		let {imagePreviewUrl} = this.state;
-		let $imagePreview = null;
-		if (imagePreviewUrl) {
-			$imagePreview = (<img src={imagePreviewUrl} />);
-
-		} else {
-
-			return (
-				<div>
-					<FeedWidgetView
-						openModal={this.state.openModal}
-						newPost={this.newPost}
-						discardPost={this.discardPost}
-						addPost={this.addPost}
-						imageUpload={this.imageUpload}
-						onDrop={this.onDrop}
-						$imagePreview={this.$imagePreviewUrl}
-					/>
-					<div className="previewText">Please select an Image for Preview</div>
-
-
-
-				</div>
-			)
-		}
-
-
-	}
-} 
+  render() {
+    return (
+      <div>
+        <FeedWidgetView
+          openModal={this.state.openModal}
+          newPost={this.newPost}
+          discardPost={this.discardPost}
+          addPost={this.addPost}
+          imageUpload={this.imageUpload}
+          onDrop={this.onDrop}
+        />
+      </div>
+    );
+  }
+}
