9ef4416ec49645a4f3894ab29e6b6639eb310d3c
Anupam Shah <anupam.shah06@gmail.com>
2020-11-17 16:20:39 +0530

    all projects frontend and backend

--- src/Pages/HackathonPage.js
+++ src/Pages/HackathonPage.js
@@ -1,12 +1,93 @@
 import React from 'react';
 
+
+import Header from '../components/Header';
+import WorkView from '../components/WorkView';
+
+
+import { Card, CardActions, CardContent, Typography} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  header: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    backgroundColor: '#f5aa0a',
+    marginBottom: '1rem',
+    height: 'fit-content',
+    padding: '0 0.3rem 0 0.3rem',
+    border: 1,
+    borderStyle: 'solid',
+    borderColor: 'grey'
+  },
+  title: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: '500',
+  }
+});
+
+const PageHeader = (props) => (
+  <Card className = {useStyles().header}>
+    <CardContent>
+      <Typography className = {useStyles().title}>
+        All Hackathons
+      </Typography>
+    </CardContent>
+  </Card>
+)
+
 export default class HackathonPage extends React.Component {
+  constructor(props)
+  {
+    super(props)
+    this.state = {
+      projects: [],
+      project: {
+        index: -1,
+        title: null,
+        description: null,
+        startDate: null,
+        endDate: null,
+        requirements: null,
+        mentor:null,
+        member: 0
+      }
+    };
+  }
+
+
+
+  ShowDetails = (index) => {
+    let pro = this.state.projects[index];
+
+    this.setState(() => ({
+      project: {
+        index: index,
+        title: pro.title,
+        description: pro.description,
+        startDate: pro.startDate,
+        endDate: pro.endDate,
+        requirements: pro.requirements,
+        mentor: pro.mentor,
+        member: pro.member
+      },
+    }))
+  }
+
+
 
   render() {
-
     return(
       <div>
-        Under Construction
+        <Header logout = {true}/>
+        <div className = "widget__list">
+        <PageHeader />
+          <WorkView
+            works={this.state.projects}
+            ShowDetails={this.ShowDetails}
+          />
+        </div>
       </div>
     )
   }
--- src/Pages/ProjectsPage.js
+++ src/Pages/ProjectsPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import Header from '../components/Header';
 import WorkView from '../components/WorkView';
-
+import {postRequest} from '../components/CallApi'
 
 import { Card, CardActions, CardContent, Typography} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
@@ -43,10 +43,6 @@ export default class ProjectsPage extends React.Component {
     super(props)
     this.state = {
       projects: [],
-      openModal: false,
-      addDetail: false,
-      showDetail: false,
-      editDetail: false,
       project: {
         index: -1,
         title: null,
@@ -58,6 +54,38 @@ export default class ProjectsPage extends React.Component {
         member: 0
       }
     };
+
+    const url = window.location.href;
+    const parser = require('url-parameter-parser');
+    const res = parser(url);
+    const user = res.email;
+
+    postRequest('project/fetchallprojects',
+      {
+        'email': user,
+      },
+      (res)=>{
+        if(res.message=="SUCCESS")
+        {
+          let default_projects = []
+          res.return_value.forEach((item)=>{
+            default_projects.push({
+              title : item.title,
+              description: item.description,
+              startDate: item.startDate,
+              endDate: item.endDate,
+              requirements: item.skills_required,
+              member: item.members,
+              mentor: item.mentor,
+              colab: item.colab,
+              project_id: item.project_id
+            })
+          })
+          console.log(default_projects)
+          this.setState({projects: default_projects})
+        }
+      }
+    )
   }
 
 
@@ -88,20 +116,11 @@ export default class ProjectsPage extends React.Component {
       <div>
         <Header logout = {true}/>
         <div className = "widget__list">
-        <PageHeader newProject = {this.showDetail} />
+        <PageHeader />
           <WorkView
             works={this.state.projects}
             ShowDetails={this.ShowDetails}
           />
-          {
-            (this.state.openModal)?
-            <ProjectModal
-              showDetail = {this.state.showDetail}
-              project = {this.state.project}
-            />
-            :
-            ''
-          }
         </div>
       </div>
     )
