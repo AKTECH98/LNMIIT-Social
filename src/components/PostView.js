896e9df233e4d9f9db7695bf9b24c5225b3e226f
tarungupta1999 <17ucs168@lnmiit.ac.in>
2020-11-18 16:11:23 +0530

    image in post working

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import {postRequest} from './CallApi';
-import SinglePostView from './SinglePostView';
+import { postRequest } from "./CallApi";
+import SinglePostView from "./SinglePostView";
 
-export default class PostView extends React.Component{
-  constructor(props)
-  {
-    super(props)
+export default class PostView extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      posts:[{user_name:'default name', content:'Dummy Post for debugging', date_of_post:'Default Date'}]
-    }
+      posts: [
+        {
+          user_name: "default name",
+          content: "Dummy Post for debugging",
+          date_of_post: "Default Date",
+        },
+      ],
+    };
   }
 
-  render(){
-    postRequest('posts/fetchposts',
+  render() {
+    postRequest(
+      "posts/fetchposts",
       {
-        'email':window.localStorage.getItem('email'),
-        'password':window.localStorage.getItem('password')
+        email: window.localStorage.getItem("email"),
+        password: window.localStorage.getItem("password"),
       },
-      (res)=>{this.setState({posts:res.results})}
-    )
-  return (
-    <div>
-    {this.state.posts==undefined?'':this.state.posts.map((item,index)=>
-      <div key = {index}>
-      <SinglePostView item = {item} />
+      (res) => {
+        this.setState({ posts: res.results });
+      }
+    );
+    return (
+      <div>
+        {this.state.posts == undefined
+          ? ""
+          : this.state.posts.map((item, index) => (
+              <div key={index}>
+                <SinglePostView item={item} />
+              </div>
+            ))}
       </div>
-    )}
-    </div>
-  )}
+    );
+  }
 }
--- src/components/SinglePostView.js
+++ src/components/SinglePostView.js
@@ -1,64 +1,79 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ReactHtmlParser from "react-html-parser";
+import { postRequest } from "./CallApi";
+import imageStyles from "./css/SinglePostView.module.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     border: 0.5,
-    borderStyle: 'solid',
-    borderColor: 'grey',
-    height: 'fit-content',
-    textDecoration: 'none',
+    borderStyle: "solid",
+    borderColor: "grey",
+    height: "fit-content",
+    textDecoration: "none",
     marginBottom: 10,
-    color: 'black'
+    color: "black",
   },
   title: {
-    fontSize : 20
+    fontSize: 20,
   },
   subheader: {
-    color: 'gray',
-    fontSize : 15
+    color: "gray",
+    fontSize: 15,
   },
   content: {
-    fontSize: 15
+    fontSize: 15,
   },
-  avatar:{
-    backgroundColor: 'purple'
+  avatar: {
+    backgroundColor: "purple",
   },
   rootIcon: {
-    color: 'blue'
+    color: "blue",
   },
 }));
 
-export default function SinglePostView(props){
-	const classes = useStyles();
+export default function SinglePostView(props) {
+  const classes = useStyles();
+  const content = props.item.content;
+  console.log(content, "content");
+  var indexOfLocalhost = content.indexOf("localhost");
+  console.log(indexOfLocalhost);
+  var post = "";
 
-	return(
-		<Card className={classes.root}>
-		<CardHeader 
-			classes = {
-			{
-				title : classes.title,
-				subheader : classes.subheader
-			}}
-      avatar={
-        <Avatar>
-          {props.item.initials}
-        </Avatar>
-      }
-      title={props.item.user_name}
-      subheader={"Date On: " + props.item.date_of_post}
-    />
-    <CardContent classes = {{root: classes.content}}>
-      {props.item.content}
-    </CardContent>
-  	</Card>
-	)
+  if (indexOfLocalhost === -1) {
+    post = content;
+  } else {
+    post = content.replace("localhost", "http://localhost");
+  }
+
+  const parsedPost = ReactHtmlParser(post);
+  console.log("parsedPost", parsedPost);
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        classes={{
+          title: classes.title,
+          subheader: classes.subheader,
+        }}
+        avatar={<Avatar>{props.item.initials}</Avatar>}
+        title={props.item.user_name}
+        subheader={"Date On: " + props.item.date_of_post}
+      />
+      <CardContent
+        classes={{ root: classes.content }}
+        className={imageStyles.image}
+      >
+        {parsedPost}
+      </CardContent>
+    </Card>
+  );
 }
