9727a608efb9224994bedd3635719df3dbf76099
Tarun Gupta <32018268+tarungupta1999@users.noreply.github.com>
2020-12-19 01:36:47 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,22 @@
-import React,{useState} from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import TextField from '@material-ui/core/TextField';
-import {postRequest} from './CallApi'
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import { postRequest } from "./CallApi";
 
-export default function Header(props){
-
-  const[searchTerm,setSearchTerm]  = useState("Test");
+export default function Header(props) {
+  const [searchTerm, setSearchTerm] = useState("Test");
 
   return (
-      <div>
-        <TextField placeholder="Search results logged in console" onChange={(e)=>{setSearchTerm(e.target.value)}}/>
-        <Link to={"/Search?search="+searchTerm}>
-          <button>
-            Search
-          </button>
-        </Link>
-      </div>
+    <div>
+      <TextField
+        placeholder='Search results logged in console'
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <Link to={"/Search?search=" + searchTerm} className='search__button'>
+        <button>Search</button>
+      </Link>
+    </div>
   );
 }
