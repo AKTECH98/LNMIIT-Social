0faad3c0860bd0d40b89a19612ec171258d842a4
Tarun Gupta <32018268+tarungupta1999@users.noreply.github.com>
2020-12-23 01:39:21 +0530

    Update Personal.js

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,81 +1,98 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardContent, CardHeader } from '@material-ui/core';
-import { Typography } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardContent, CardHeader } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList'
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   menuroot: {
-    color: 'black',
-    fontSize: 12
+    color: "black",
+    fontSize: 12,
   },
   root: {
-    backgroundColor: 'white',
-    height: 'fit-content',
-    minWidth: 850
+    backgroundColor: "white",
+    height: "fit-content",
+    minWidth: 850,
   },
   rootHeader: {
-    height: 'fit-content'
+    height: "fit-content",
   },
   subHeader: {
     fontSize: 15,
-    fontColor: 'black'
+    fontColor: "black",
   },
   subRootA: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     minWidth: 400,
-    Height: 'fit-content',
-    marginRight: 10
+    Height: "fit-content",
+    marginRight: 10,
   },
-  subRootB:{
-    backgroundColor: 'white',
-    Height: 'fit-content',
+  subRootB: {
+    backgroundColor: "white",
+    Height: "fit-content",
     minWidth: 200,
-    marginLeft: 10
+    marginLeft: 10,
   },
   subInfo: {
-    borderBottom: '0.1rem solid grey',
+    borderBottom: "0.1rem solid grey",
     fontSize: 12,
-    fontColor: 'black'
+    fontColor: "black",
   },
   title: {
     fontSize: 40,
-    color: '#4574bf',
+    color: "#4574bf",
     fontWeight: 700,
-    fontFamily: 'cursive'
+    fontFamily: "cursive",
   },
   subTitle: {
     fontSize: 25,
-    color: '#4574bf',
-    fontWeight: 400
+    color: "#4574bf",
+    fontWeight: 400,
   },
   content: {
-    display: 'flex',
-    justifyContent: 'space-between'
+    display: "flex",
+    justifyContent: "space-between",
   },
   subContent: {
-    color: '#4574bf',
+    color: "#4574bf",
     fontSize: 15,
-    marginBottom: 5
+    marginBottom: 5,
   },
   rootIcon: {
-    color: 'blue'
-  }
+    color: "blue",
+  },
+  "@media (max-width: 768px)": {
+    root: {
+      minWidth: "70vw",
+    },
+    content: {
+      flexDirection: "column",
+      width: "90vw",
+      padding: "5px",
+    },
+    subRootB: {
+      marginLeft: 0,
+      marginTop: 10,
+      minWidth: "10vw",
+    },
+    subRootA: {
+      minWidth: "10vw",
+    },
+  },
 });
 
-export default function Personal(props){
-
+export default function Personal(props) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
@@ -93,7 +110,7 @@ export default function Personal(props){
   };
 
   function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
+    if (event.key === "Tab") {
       event.preventDefault();
       setOpen(false);
     }
@@ -109,108 +126,125 @@ export default function Personal(props){
 
     prevOpen.current = open;
   }, [open]);
-  
-  return(
-  <Card className = {classes.root}>
-    <CardHeader 
-      classes = {
-        {
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        classes={{
           root: classes.rootHeader,
-          title : classes.title,
-          subheader: classes.subInfo
+          title: classes.title,
+          subheader: classes.subInfo,
+        }}
+        action={
+          props.view ? (
+            ""
+          ) : (
+            <div>
+              <IconButton
+                ref={anchorRef}
+                aria-controls={open ? "menu-list-grow" : undefined}
+                aria-haspopup='true'
+                onClick={handleToggle}
+                classes={{ root: classes.rootIcon }}
+                disabled={props.view}
+              >
+                <MoreVertIcon />
+              </IconButton>
+              <Popper
+                open={open}
+                anchorEl={anchorRef.current}
+                role={undefined}
+                transition
+                disablePortal
+              >
+                {({ TransitionProps, placement }) => (
+                  <Grow
+                    {...TransitionProps}
+                    style={{
+                      transformOrigin:
+                        placement === "bottom" ? "center top" : "center bottom",
+                    }}
+                  >
+                    <Paper>
+                      <ClickAwayListener onClickAway={handleClose}>
+                        <MenuList
+                          autoFocusItem={open}
+                          id='menu-list-grow'
+                          onKeyDown={handleListKeyDown}
+                        >
+                          <Link to={"EditProfile"} className='linklink'>
+                            <MenuItem
+                              onClick={handleClose}
+                              className={classes.menuroot}
+                            >
+                              Edit
+                            </MenuItem>
+                          </Link>
+                        </MenuList>
+                      </ClickAwayListener>
+                    </Paper>
+                  </Grow>
+                )}
+              </Popper>
+            </div>
+          )
         }
-      }
+        title={
+          props.personal == null
+            ? "Default Name"
+            : props.personal.first_name +
+              " " +
+              props.personal.middle_name +
+              " " +
+              props.personal.last_name
+        }
+        subheader='Competitive Coder/Designer/Machine Learning'
+      />
+      <CardContent classes={{ root: classes.content }}>
+        <Card className={classes.subRootA}>
+          <CardHeader
+            classes={{
+              root: classes.subHeader,
+              title: classes.subTitle,
+              subheader: classes.subInfo,
+            }}
+            title='About'
+            subheader='1st/2nd/3rd/4th Year/Alumni (Y16 Batch)'
+          />
+          <CardContent classes={{ root: classes.subContent }}>
+            {props.personal == null
+              ? "Default Description"
+              : props.personal.profile_description}
+          </CardContent>
+        </Card>
 
-      action={
-        props.view?"":<div>
-        <IconButton
-          ref={anchorRef}
-          aria-controls={open ? 'menu-list-grow' : undefined}
-          aria-haspopup="true"
-          onClick={handleToggle}
-          classes = {{root: classes.rootIcon}}
-          disabled = {props.view}
-        >
-          <MoreVertIcon />
-        </IconButton>
-        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
-          {({ TransitionProps, placement }) => (
-            <Grow
-              {...TransitionProps}
-              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-            >
-            <Paper>
-              <ClickAwayListener onClickAway={handleClose}>
-                <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                  <Link to={'EditProfile'} className = "linklink">
-                  <MenuItem onClick = {handleClose} className = {classes.menuroot}>
-                    Edit
-                  </MenuItem>
-                  </Link>
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
-            </Grow>
-          )}
-        </Popper>
-        </div>
-      }  
-      
-      title= {props.personal==null?'Default Name': props.personal.first_name + ' ' + props.personal.middle_name + ' ' + props.personal.last_name}
-      subheader = "Competitive Coder/Designer/Machine Learning"
-    />
-    <CardContent classes = {{root:classes.content}}>
-
-      <Card className = {classes.subRootA}>
-        <CardHeader
-          classes = {{
-            root: classes.subHeader,
-            title: classes.subTitle,
-            subheader: classes.subInfo
-          }}
-          title= "About"
-          subheader = "1st/2nd/3rd/4th Year/Alumni (Y16 Batch)"
-        />
-        <CardContent classes = {{root:classes.subContent}}>
-          {props.personal==null?'Default Description':props.personal.profile_description}
-        </CardContent>
-      </Card>
-
-      <Card className = {classes.subRootB}>
-        <CardHeader
-          classes = {{
-            root: classes.subHeader,
-            title: classes.subTitle,
-            subheader: classes.subInfo
-          }}
-          title= "Contact Info. & Links"
-          subheader = "."
-        />
-        <CardContent classes = {{root:classes.subContent}}>
-          <Typography variant = "h5">
-            Phone No.  : {props.personal==null?'Default Phone':props.personal.phone}
-          </Typography>
-          <Typography variant = "h5">
-            Email-ID   : {props.personal==null?'Default Email':props.personal.email}
-          </Typography>
-          <Typography variant = "h5">
-            Linkdin    : none
-          </Typography>
-          <Typography variant = "h5">
-            Github     : none
-          </Typography>
-          <Typography variant = "h5">
-            Codechef   : none
-          </Typography>
-          <Typography variant = "h5">
-            Codeforces : none
-          </Typography>
-          <Typography variant = "h5">
-            Other      : none
-          </Typography>
-        </CardContent>
-      </Card>
-
-    </CardContent>
-  </Card>
-)}
+        <Card className={classes.subRootB}>
+          <CardHeader
+            classes={{
+              root: classes.subHeader,
+              title: classes.subTitle,
+              subheader: classes.subInfo,
+            }}
+            title='Contact Info. & Links'
+            subheader='.'
+          />
+          <CardContent classes={{ root: classes.subContent }}>
+            <Typography variant='h5'>
+              Phone No. :{" "}
+              {props.personal == null ? "Default Phone" : props.personal.phone}
+            </Typography>
+            <Typography variant='h5'>
+              Email-ID :{" "}
+              {props.personal == null ? "Default Email" : props.personal.email}
+            </Typography>
+            <Typography variant='h5'>Linkdin : none</Typography>
+            <Typography variant='h5'>Github : none</Typography>
+            <Typography variant='h5'>Codechef : none</Typography>
+            <Typography variant='h5'>Codeforces : none</Typography>
+            <Typography variant='h5'>Other : none</Typography>
+          </CardContent>
+        </Card>
+      </CardContent>
+    </Card>
+  );
+}
