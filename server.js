247ca64a93dd40fe7dc46372a883fe691165f325
anshulkiyawat <17ucs030@lnmiit.ac.in>
2021-01-11 19:03:14 +0530

    Fix 2

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 8080);
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
